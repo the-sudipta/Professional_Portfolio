a861a8bbb9b5153661c9949912278848a4b0c2c7
Sudipta Desktop BD <engineer.sudipta.kumar.das@gmail.com>
2025-03-26 19:58:10 +0600

    🎨 ui: Update CDN for Ion Icons
    Updated the Content Delivery Network (CDN) link for Ion Icons to ensure better performance, reliability, and compatibility with the latest version.

--- view/index.php
+++ view/index.php
@@ -31,6 +31,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/7.4.0/ionicons.min.js"></script>
+
 
 
     <style>
